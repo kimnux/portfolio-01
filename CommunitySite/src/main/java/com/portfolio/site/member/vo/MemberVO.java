fc2505dd8a156771f28c3af0f0840ec7ab8c6442
kimnux <slasnrndu@gmail.com>
2021-07-11 21:23:22 +0900

    기본셋팅 UTF8 Encoding, DB연동(mariaDB), tiles셋팅

---
+++ CommunitySite/src/main/java/com/portfolio/site/member/vo/MemberVO.java
@@ -0,0 +1,14 @@
+package com.portfolio.site.member.vo;
+
+import lombok.Data;
+
+@Data
+public class MemberVO {
+	private int idx;
+	private String userId;
+	private String password;
+	private String nickName;
+	private String grade;
+	
+	private String regDt;
+}
